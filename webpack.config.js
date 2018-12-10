d2a81146e2a62d29f655296b8167a0a71c71f9d2
Alexander Zakharov <alzak@envisage.no>
2018-12-10 21:56:48 +0100

    Added babel and removed .json from rc file names.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
   output: {
     filename: 'bundle.min.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        loader: 'babel-loader'
+      }
+    ]
   }
 };
