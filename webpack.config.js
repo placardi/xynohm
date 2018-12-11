dc9f461cba6583c0ef07ac0e5f305e43ef99b93e
Alexander Zakharov <alzak@envisage.no>
2018-12-11 19:40:35 +0100

    More configuration setup.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require('path');
+const library = require('./package.json').name;
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.min.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/, /dist/],
-        loader: 'babel-loader'
-      }
-    ]
-  }
+module.exports = env => {
+  const isDevelopment = env.mode === 'development' || false;
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'source-map' : 'none',
+    entry: './src/index.ts',
+    output: {
+      filename: library + (isDevelopment ? '' : '.min') + '.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: library,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    devServer: {
+      contentBase: './dist'
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        }
+      ]
+    }
+  };
 };
