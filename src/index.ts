8b1aa8f45fd04b9f40e6bbb1df978fda5ee70db3
Alexander Zakharov <alzak@envisage.no>
2018-12-29 17:25:50 +0100

    Added main entry point.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { App } from './core/app';
+import { Component } from './core/component';
+import { Module } from './core/module';
+import { Resolver } from './core/resolver';
+
+export { App, Component, Module, Resolver };
