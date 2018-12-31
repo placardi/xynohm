98bfdffd1164e7cdfdde60cd9d4cd09cd983fd9d
Alexander Zakharov <alzak@envisage.no>
2018-12-31 15:59:26 +0100

    Modified release script.

--- bin/release.sh
+++ bin/release.sh
@@ -16,31 +16,30 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   masterBranch=master
   releaseBranch=release-v$VERSION
 
-  git push origin --delete release
-
   git checkout -b $releaseBranch $developBranch
 
   npm --no-git-tag-version version "$VERSION"
 
-  git add package.json package-lock.json npm-shrinkwrap.json
+  git add package.json package-lock.json
   git commit --no-verify -m "Bumped version number to $VERSION."
 
   git checkout $masterBranch
   git merge --no-ff --no-edit $releaseBranch
 
-  git tag -a $versionLabel -m "Version $VERSION tag."
+  git tag -a $VERSION -m "Version $VERSION tag."
 
   git push --follow-tags
 
+  npm run build:prod
   npm publish --access public
 
   git checkout $developBranch
   git merge --no-ff --no-edit $releaseBranch
 
-  SNAPSHOT_VERSION=$(npm --no-git-tag-version version minor)
-  sed  -i '/version/s/[^.]*$/'"${SNAPSHOT_VERSION}\"/" package.json
+  SNAPSHOT_VERSION=$(npm --no-git-tag-version version patch)
+  sed -E -i '' 's/"version": ".+"/"version": "'${SNAPSHOT_VERSION}'-SNAPSHOT"/' package.json
 
-  git add package.json
+  git add package.json package-lock.json
   git commit --no-verify -m "Bumped snapshot version number."
 
   git push
