fd6f2a6e93b226e27077aebc4efda10be6dbe963
Alexander Zakharov <alzak@envisage.no>
2019-01-01 18:49:05 +0100

    Minir project configuration fixes.

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -3,11 +3,5 @@ const baseConfig = require('./base.config.js');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
-  devtool: 'none',
-  devServer: {
-    contentBase: './tmp',
-    host: '0.0.0.0',
-    port: 4200,
-    historyApiFallback: true
-  }
+  devtool: 'none'
 });
