45467fa91b9aae789d09e1a2b858c89ec23dd2c5
Alexander Zakharov <alzak@envisage.no>
2019-01-12 18:26:30 +0100

    Added changes to the rest of the library to make use of new AppRoot and RouterOutlet modules.

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,21 +1,21 @@
 import { AppInterface } from '../types/app';
 import { ComponentDefinition } from '../types/component';
 import { Configuration } from '../types/configuration';
-import { ModuleDefinition, ModuleIntrface } from '../types/module';
-import { ResolverDefinition, ResolverIntrface } from '../types/resolver';
 import {
   RouteDefinitionInterface,
   RouteInterface,
   RouterInterface
 } from '../types/router';
+import { AppRoot } from './app-root';
 import { Router } from './router';
+import { RouterOutlet } from './router-outlet';
 
 export class App implements AppInterface {
   private components: ComponentDefinition[];
   private configuration: Configuration;
   private router: RouterInterface;
-  private appRoot: HTMLElement;
-  private routerOutlet: HTMLElement;
+  private appRoot: AppRoot;
+  private routerOutlet: RouterOutlet;
 
   constructor(
     components: ComponentDefinition[],
@@ -24,8 +24,8 @@ export class App implements AppInterface {
   ) {
     this.components = components;
     this.configuration = configuration;
-    this.appRoot = this.getAppRoot();
-    this.routerOutlet = this.getRouterOutlet();
+    this.appRoot = new AppRoot();
+    this.routerOutlet = new RouterOutlet(this.appRoot);
     this.router = new Router(
       this.initRoutes(routeDefinitions),
       this.configuration,
@@ -34,6 +34,7 @@ export class App implements AppInterface {
   }
 
   public run(): void {
+    this.setBaseHref();
     document.addEventListener('DOMContentLoaded', () => this.router.navigate());
   }
 
@@ -41,40 +42,54 @@ export class App implements AppInterface {
     routeDefinitions: RouteDefinitionInterface[]
   ): RouteInterface[] {
     return routeDefinitions.map(definition => {
+      if (!this.isValidRouteDefinition(definition)) {
+        throw new Error(
+          `invalid route definition with name: ${definition.name} and path: ${
+            definition.path
+          }`
+        );
+      }
       return {
         name: definition.name,
         path: definition.path,
-        module: this.instantiateModule(definition.module),
-        resolver: this.instantiateResolver(definition.resolver)
+        module: new definition.module(
+          this.components,
+          this.configuration,
+          this.routerOutlet
+        ),
+        resolver:
+          (definition.resolver && new definition.resolver()) || undefined
       };
     });
   }
 
-  private instantiateModule(module: ModuleDefinition): ModuleIntrface {
-    return new module(this.components, this.configuration, this.routerOutlet);
-  }
-
-  private instantiateResolver(
-    resolver: ResolverDefinition | undefined
-  ): ResolverIntrface | undefined {
-    return !!resolver ? new resolver() : undefined;
-  }
-
-  private getAppRoot(): HTMLElement {
-    const appRoot: HTMLElement | null = document.querySelector('app-root');
-    if (!appRoot) {
-      throw new Error('app-root element not found');
-    }
-    return appRoot;
-  }
-
-  private getRouterOutlet(): HTMLElement {
-    const routerOutlet: HTMLElement | null = this.appRoot.querySelector(
-      'router-outlet'
+  private isValidRouteDefinition(
+    definition: RouteDefinitionInterface
+  ): boolean {
+    return (
+      typeof definition.name === 'string' &&
+      definition.name.length > 0 &&
+      typeof definition.path === 'string' &&
+      definition.path.length > 0 &&
+      (definition.path.charAt(0) === '/' || definition.path === '**') &&
+      !!definition.module &&
+      definition.module.prototype.__proto__.name === 'Module' &&
+      (!!definition.resolver
+        ? definition.resolver.prototype.__proto__.name === 'Resolver' &&
+          definition.resolver.prototype.resolve
+        : true)
     );
-    if (!routerOutlet) {
-      throw new Error('router-outlet element in app-root not found');
+  }
+
+  private setBaseHref() {
+    let base: Element = document.getElementsByTagName('base')[0];
+    if (!base) {
+      base = document.createElement('base');
+      document.head.insertBefore(base, document.head.firstChild);
     }
-    return routerOutlet;
+    base.setAttribute(
+      'href',
+      location.origin + (this.configuration.baseHref || '')
+    );
   }
 }
