44a9e4b0789c50c6cb75cbc9898e32063669cdc9
Alexander Zakharov <alzak@envisage.no>
2019-01-20 02:16:12 +0100

    Added missing module.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './types/index';
 export * from './core/app';
 export * from './core/component';
+export * from './core/guard';
 export * from './core/module';
 export * from './core/resolver';
