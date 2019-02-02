2ea250baf527040f16dc5b595cfb8ee15955bafd
Alexander Zakharov <alzak@envisage.no>
2019-02-02 23:05:52 +0100

    Updated examples.

--- examples/counters/src/resolvers/counters.resolver.ts
+++ examples/counters/src/resolvers/counters.resolver.ts
@@ -3,6 +3,20 @@ import { Resolver } from '@placardi/xynohm';
 export class CountersResolver extends Resolver {
   public resolve(): Promise<object> {
     return Promise.resolve({
+      counters: [
+        {
+          id: 0,
+          value: 0
+        },
+        {
+          id: 1,
+          value: 1
+        },
+        {
+          id: 2,
+          value: -1
+        }
+      ],
       numberOfCounters: 3,
       button: {
         add: 'Add',
